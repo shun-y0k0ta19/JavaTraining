39cc61c86323b5b875d788ab54afe1f22350728f
y0k0ta19 <motoyk@gmail.com>
2015-05-16 22:57:31 +0900

    change gitignore

--- CH03/src/ex03_04/Answer.java
+++
@@ -1,9 +0,0 @@
-package ex03_04;
-
-public class Answer {
-
-	public static void main(String[] args) {
-		System.out.println("A: stopメソッドをfinalにし、確実にspeedを０にする実装を保証する");
-	}
-
-}
--- CH03/src/ex03_12/ObjectComparator.java
+++
@@ -1,6 +0,0 @@
-package ex03_12;
-
-public interface ObjectComparator{
-
-	abstract int compareObject(Object o1, Object o2);
-}
